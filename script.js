f9797d751e91dca7ec9e80b79d6dfb07bced83a2
IamNeger <144994502+IamNeger@users.noreply.github.com>
2024-04-18 10:53:41 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
+let score = document.getElementById("scoren")
+let scoren = 0
 let cut = false
 let guy = document.getElementById("guy")
-let treeY=20
-let forest=document.getElementById("forest")
+let treeY = 20
+let forest = document.getElementById("forest")
 document.onkeydown = function (event) {
     if (event.code == "KeyA") {
         guy.style.transform = "translate(-100%,-50%)"
@@ -12,25 +14,43 @@ document.onkeydown = function (event) {
         console.log(event.code);
     }
     if (event.code == "Space") {
+
         if (cut == false) {
-            let trees=document.getElementsByClassName("tree")
-            trees[0].remove()
-            guy.src = "Untitled-3.png"
+            scoren = scoren + 1
+            score.innerHTML = "score: " + scoren
+            newTree()
             setTimeout(function () {
-                guy.src = "timberguy.png"
-                cut=false
-            }, 500)
-            cut = true
+
+
+                let trees = document.getElementsByClassName("tree")
+                trees[0].remove()
+                // нужно чтобы новое дерево появлялась в нужном месте
+                treeY = treeY - 99
+                guy.src = "Untitled-3.png"
+                console.log(trees);
+                // цикл опускает деревья 
+                for (let t of trees) {
+                    t.style.transform = "translate(-25%,-" + (t.classList[1] - 99) + "%)"
+                    // t.classList[1]=(t.classList[1]-99)
+                    t.classList.add(t.classList[1] - 99)
+                    t.classList.remove(t.classList[1])
+                }
+                // setTimeout(function () {
+                //     guy.src = "timberguy.png"
+                //     cut = false
+                // }, 500)
+                cut = true
+            }, 1)
         }
     }
 }
 function newTree() {
-let tree=document.createElement("img")
-forest.appendChild(tree)
-tree.src="tree"+Math.floor(Math.random()*2)+".png"
-tree.className="tree"
-tree.style.transform="translate(-25%,-"+treeY+"%)"
-treeY=treeY+99
+    let tree = document.createElement("img")
+    forest.appendChild(tree)
+    tree.src = "tree" + Math.floor(Math.random() * 2) + ".png"
+    tree.className = "tree " + treeY
+    tree.style.transform = "translate(-25%,-" + treeY + "%)"
+    treeY = treeY + 99
 }
 newTree()
 newTree()
@@ -38,3 +58,9 @@ newTree()
 newTree()
 newTree()
 newTree()
+document.onkeyup = function (event) {
+    if (event.code == "Space") {
+        cut = false
+        guy.src="timberguy.png"
+    }
+}
